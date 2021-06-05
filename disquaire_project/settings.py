3027ab937dfe35fa635b4b178a53999e43accc62
fernandes-stephane <stephfer@outlook.fr>
2021-06-05 23:26:32 +0200

    change directory

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -30,7 +30,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['https://diskaire.herokuapp.com/']
+ALLOWED_HOSTS = ['diskaire.herokuapp.com/']
 
 
 # Application definition
