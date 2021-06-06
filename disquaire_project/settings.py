69a41d8a7f52f2ff1811c28f9b513cbfb9287115
fernandes-stephane <stephfer@outlook.fr>
2021-06-06 03:35:27 +0200

    essai numero 1

--- disquaire_project/settings.py
+++ disquaire_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+# Configure Django App for Heroku.
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +155,5 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+
+    django_heroku.settings(locals())
